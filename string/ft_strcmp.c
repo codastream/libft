fc4fb251e0ca6b35d260227bafb840b7cc6390f5
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-01-23 17:19:38 +0100

    null safe strcmp

--- string/ft_strcmp.c
+++ string/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: fpetit <fpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 10:45:29 by fpetit            #+#    #+#             */
-/*   Updated: 2024/11/27 12:39:16 by fpetit           ###   ########.fr       */
+/*   Updated: 2025/01/23 17:19:02 by fpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
 
+	if (!s1)
+		return ((unsigned char) *s2);
+	if (!s2)
+		return ((unsigned char) *s1);
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 	{
